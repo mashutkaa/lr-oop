80c6d2cfb8b0f95aba30b5bea99fa5988a51d975
Mariia Hupalenko <marichka.hupalenko@gmail.com>
2024-12-26 19:43:32 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,37 +10,37 @@ namespace Game
         {
             Console.OutputEncoding = UTF8Encoding.UTF8;
 
-            GameAccount ghostRider = new GameAccount("GhostRider");
-            GameAccount iceBlade = new GameAccount("IceBlade");
+            GameAccount player1 = new GameAccount("Player1");
+            BanAccount player2 = new BanAccount("Player2");
+            PremiumAccount player3 = new PremiumAccount("Player3");
+            GameFactory gameFactory = new GameFactory();
 
-            ghostRider.WinGame(iceBlade, 20);
-            iceBlade.LoseGame(ghostRider, 20);
-            iceBlade.WinGame(ghostRider, 30);
-            iceBlade.LoseGame(ghostRider, 20);
-            iceBlade.WinGame(ghostRider, 30);
+            player2.WinGame(player1, "StandartGame");
+            player1.WinGame(player2, "StandartGame");
 
-            ghostRider.GetStats();
-            iceBlade.GetStats();
+            player1.LoseGame(player3, "StandartGame");
+            player1.LoseGame(player3, "StandartGame");
+            player1.LoseGame(player3, "StandartGame");
+
+            player2.WinGame(player1, "TrainingGame");
+            player2.WinGame(player3, "TrainingGame");
+
+            player1.GetStats();
+            player2.GetStats();
+            player3.GetStats();
         }
     }
 
     class GameAccount
     {
-        private static int _gameIDCounter = 0;
         private List<Game> _historyOfGames = new List<Game>();
         private int _currentRating;
 
         public string UserName { get; set; }
         public int CurrentRating
         {
-            get
-            {
-                return _currentRating;
-            }
-            set
-            {
-                _currentRating = (value < 1) ? 1 : value;
-            }
+            get => _currentRating;
+            set => _currentRating = Math.Max(1, value);
         }
         public int GamesCount { get; set; }
 
@@ -51,40 +51,38 @@ namespace Game
             GamesCount = 0;
         }
 
-        public void WinGame(GameAccount opponent, int rating)
+        public void WinGame(GameAccount opponent, string gameType)
         {
-            if (rating < 0)
-            {
-                throw new Exception("Від'ємний рейтинг");
-            }
-
-            this.CurrentRating += rating;
-            opponent.CurrentRating -= rating;
-
-            var game = new Game(this.UserName, opponent.UserName, rating);
-            this._historyOfGames.Add(game);
-            opponent._historyOfGames.Add(game);
-
-            this.GamesCount++;
-            opponent.GamesCount++;
+            RecordGameResult(this, opponent, gameType);
         }
 
-        public void LoseGame(GameAccount opponent, int rating)
+        public void LoseGame(GameAccount opponent, string gameType)
         {
-            if (rating < 0)
-            {
-                throw new Exception("Від'ємний рейтинг");
-            }
+            RecordGameResult(opponent, this, gameType);
+        }
 
-            this.CurrentRating -= rating;
-            opponent.CurrentRating += rating;
+        protected void RecordGameResult(GameAccount winner, GameAccount loser, string gameType)
+        {
+            Game game = GameFactory.CreateGame(gameType, winner, loser);
+            int rating = game.Rating;
 
-            var game = new Game(opponent.UserName, this.UserName, rating);
-            this._historyOfGames.Add(game);
-            opponent._historyOfGames.Add(game);
+            winner.UpdateRatingsOnWin(rating);
+            loser.UpdateRatingsOnLoss(rating);
 
-            this.GamesCount++;
-            opponent.GamesCount++;
+            winner._historyOfGames.Add(game);
+            loser._historyOfGames.Add(game);
+        }
+
+        protected virtual void UpdateRatingsOnWin(int rating)
+        {
+            CurrentRating += rating;
+            GamesCount++;
+        }
+
+        protected virtual void UpdateRatingsOnLoss(int rating)
+        {
+            CurrentRating -= rating;
+            GamesCount++;
         }
 
         public void GetStats()
@@ -116,7 +114,43 @@ namespace Game
         }
     }
 
-    class Game
+    class BanAccount : GameAccount
+    {
+        public BanAccount(string userName) : base(userName) { }
+
+        protected override void UpdateRatingsOnLoss(int rating)
+        {
+            base.UpdateRatingsOnLoss(rating);
+            CurrentRating -= rating;
+        }
+    }
+
+    class PremiumAccount : GameAccount
+    {
+        private int _countWins = 0;
+
+        public PremiumAccount(string userName) : base(userName) { }
+
+        protected override void UpdateRatingsOnWin(int rating)
+        {
+            base.UpdateRatingsOnWin(rating);
+            _countWins++;
+
+            if (_countWins == 3)
+            {
+                CurrentRating += 20;
+                _countWins = 0;
+            }
+        }
+
+        protected override void UpdateRatingsOnLoss(int rating)
+        {
+            base.UpdateRatingsOnLoss(rating);
+            _countWins = 0;
+        }
+    }
+
+    abstract class Game
     {
         private static int _gameIDCounter = 0;
 
@@ -125,12 +159,49 @@ namespace Game
         public string Loser { get; private set; }
         public int Rating { get; private set; }
 
-        public Game(string winner, string loser, int rating)
+        public Game(GameAccount winner, GameAccount loser)
         {
             GameID = ++_gameIDCounter;
-            Winner = winner;
-            Loser = loser;
-            Rating = rating;
+            Winner = winner.UserName;
+            Loser = loser.UserName;
+            Rating = GenerateRating();
+        }
+
+        protected abstract int GenerateRating();
+    }
+
+    class StandartGame : Game
+    {
+        private static readonly Random _random = new Random();
+
+        public StandartGame(GameAccount winner, GameAccount loser) : base(winner, loser) { }
+
+        protected override int GenerateRating()
+        {
+            return _random.Next(20, 101);
+        }
+    }
+
+    class TrainingGame : Game
+    {
+        public TrainingGame(GameAccount winner, GameAccount loser) : base(winner, loser) { }
+
+        protected override int GenerateRating()
+        {
+            return 0;
+        }
+    }
+
+    class GameFactory
+    {
+        public static Game CreateGame(string gameType, GameAccount winner, GameAccount loser)
+        {
+            return gameType switch
+            {
+                "StandartGame" => new StandartGame(winner, loser),
+                "TrainingGame" => new TrainingGame(winner, loser),
+                _ => throw new ArgumentException("Invalid game type")
+            };
         }
     }
 }
